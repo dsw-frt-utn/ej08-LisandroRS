5407fdd39a262324c4b057b61f6a8d2f0b79a506
Lisandro <lisandrorivassunen113@gmail.com>
2026-05-04 00:12:21 -0300

    Modificacion del README y Problema 1 resuelto

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*Crear un método público ObtenerEtiquetaProducto dentro de una clase parcial
+ProductHelper.
+El método debe recibir:
+● long code
+● string description
+● decimal price
+Debe retornar una cadena con el formato:
+[code] description - price
+El precio debe mostrarse en formato moneda.
+Condiciones:
+● La clase ProductHelper debe estar dividida en dos archivos usando partial.
+● En un archivo debe ubicarse el método ObtenerEtiquetaProducto.
+● En el otro archivo debe ubicarse al menos otro método auxiliar o constante utilizada
+por el método principal.
+*/
+
+namespace Dsw2026Ej8
+{
+
+	public partial class ProductHelper
+    {
+		public string ObtenerEtiquetaProducto (long code, string description, decimal price)
+		{
+
+			string priceFormato = formatearMoneda(price);
+            return $"[{code}] {description} - {priceFormato}";
+        }
+	}
+
+}
---
+++ Dsw2026Ej8/ProductHelperExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dsw2026Ej8
+{
+
+    public partial class ProductHelper
+    {
+        private string formatearMoneda (decimal price)
+        {
+            return price.ToString("C");
+        }
+    }
