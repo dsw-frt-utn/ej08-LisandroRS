cbe9b2a2b8433708e2f0909075d2711cb3f5b0e2
Lisandro <lisandrorivassunen113@gmail.com>
2026-05-04 12:27:17 -0300

    Correccion del commit del Problema 6

--- Dsw2026Ej8/Extensions.cs
+++ Dsw2026Ej8/Extensions.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 
-public class Class1
+namespace Dsw2026Ej8
 {
-	public Class1()
-	{
-	}
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+            {
+                return "SIN-CODIGO";
+            }
+
+            return code.Trim().ToUpper().Replace(" ", "-");
+        }
+    }
 }
--- Dsw2026Ej8/Problema6.cs
+++ Dsw2026Ej8/Problema6.cs
@@ -1,8 +1,28 @@
 ﻿using System;
 
-public class Class1
+/*Problema N° 6
+Crear un método público NormalizarCodigoProducto, dentro de una clase Problema6, que reciba:
+● string code
+Debe retornar un string.
+Condiciones:
+● El método debe usar un método de extensión llamado ToProductCode.
+● El método de extensión se debe crear en otra clase, en otro archivo, denominada Extensions
+● El método de extensión debe:
+○ eliminar espacios al inicio y al final,
+○ convertir el texto a mayúsculas,
+○ reemplazar espacios internos por guiones.
+● Si el código es null, debe retornar "SIN-CODIGO".
+Ejemplo:
+" ab 123 x " => "AB-123-X"
+*/
+
+namespace Dsw2026Ej8
 {
-	public Class1()
-	{
-	}
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            return code.ToProductCode();
+        }
+    }
 }
